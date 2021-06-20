d6987699c597e4d264451b66467514fc5bf1508f
chiakaiming <chiakaiming@hotmail.com>
2021-06-20 16:42:35 +0800

    still building on actions and reducers

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useSelector, useDispatch} from "react-redux";
+import {getTasks} from "../../store/actions/task.action";
 
 function Dashboard(props) {
+    const [taskList, setTaskList] = useState([])
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(getTasks())
+    },[])
+    let store = useSelector(state => state)
+    console.log(store)
+
     return (
         <>
         <div>Dashboard</div>
-    <div>Dashboard</div>
-    <div>Dashboard</div>
+        <div>Dashboard</div>
+        <div>Dashboard</div>
         </>
     );
 }
--- src/store/reducers/task.reducer.js
+++ src/store/reducers/task.reducer.js
@@ -1,11 +1,15 @@
 
 let initialState = {
-    tasks: []
+    tasks: [{
+        name: "test",
+        description: "what is redux, what is life?"
+    }]
 }
 
 export function taskReducer(state=initialState, action){
     switch (action.type){
         case "GET_TASKS":
+            console.log("get tasks reducer hit")
             return{
                 ...state
             }
