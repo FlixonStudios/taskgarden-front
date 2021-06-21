d4f9ec0c20faefa72741a0f160d5c5f51daa8ccd
chiakaiming <chiakaiming@hotmail.com>
2021-06-21 11:55:17 +0800

    resolved merge conflicts

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,32 +1,17 @@
 import React, {useEffect, useState} from 'react';
-<<<<<<< HEAD
-import {useSelector, useDispatch} from "react-redux";
-import {getTaskList} from "../../store/actions/task.action";
-
-function Dashboard(props) {
-    const [taskList, setTaskList] = useState([])
-    const dispatch = useDispatch()
-
-    useEffect(()=>{
-        dispatch(getTaskList())
-    },[])
-    let store = useSelector(state => state)
-    console.log(store)
-
-    return (
-        <>
-        <div>Dashboard</div>
-        <div>Dashboard</div>
-        <div>Dashboard</div>
-=======
 import {Button, Col, Container, Row, Toast} from "react-bootstrap";
 import AddTask from "./AddTask";
 import axios from "axios";
+import {useSelector, useDispatch} from "react-redux";
+import {getTaskList} from "../../store/actions/task.action";
+
+const dispatch = useDispatch()
 
 function Dashboard(props) {
     // Add Task Modal
     const [addTaskShow, setAddTaskShow] = useState(false); // Modal appearance state
     const handleShow = () => setAddTaskShow(true); // Function to show Modal
+    let store = useSelector(state => state)
 
     // Tasks State
     const [tasks, setTasks] = useState([])
@@ -45,6 +30,7 @@ function Dashboard(props) {
     }
 
     useEffect(() => {
+        dispatch(getTaskList())
         getTasks()
     }, [])
 
@@ -69,7 +55,7 @@ function Dashboard(props) {
                     ))}
                 </Row>
             </Container>
->>>>>>> 32c070a36143db25d57330dd5b825a4ad5f96876
+
         </>
     );
 }
