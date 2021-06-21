d54226b8ff59910f3dd7e69b0a83f26d21bf5c47
chiakaiming <chiakaiming@hotmail.com>
2021-06-21 15:44:41 +0800

    created Task board and Sub Board

---
+++ src/components/dashboard/SubBoard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SubBoard(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default SubBoard;
---
+++ src/components/dashboard/Taskboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Taskboard(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Taskboard;
