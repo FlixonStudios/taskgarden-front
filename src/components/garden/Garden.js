49d067b4b073b0f63f45b29baf8ab32b20ba3fcc
yjrandom <random.ran.dom@hotmail.com>
2021-06-23 16:13:26 +0800

    Basic Component

--- src/components/garden/Garden.js
+++ src/components/garden/Garden.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import {Col, Container} from "react-bootstrap";
+import {Accordion, Card, Col, Container, Button, Row} from "react-bootstrap";
 
-function Garden(props) {
+function Garden({user}) {
     return (
-        <Container>
-            <h2>Garden</h2>
-            <Col>
-                <div>Inventory</div>
-            </Col>
-        </Container>
+        <Container className="my-5">
+            <Row>
+                <Col md={3}>
+        <Accordion defaultActiveKey="0">
+            <Card>
+                <Card.Header>
+                    <Accordion.Toggle as={Button} variant="primary" eventKey="0">
+                        Inventory
+                    </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                        {user.plants.map(id => (
+                        <Container key={id} style={{backgroundColor: "green"}} className="p-5 my-4">
+                            {id}
+                        </Container>
+                        ))}
+                        <Container style={{backgroundColor: "green"}} className="p-5 my-4">
 
+                        </Container>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
+                </Col>
+                <Col md={9}>
+                    <Container style={{height: "70vh", backgroundColor: "green"}}>
+
+                    </Container>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
